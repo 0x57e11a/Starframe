21e63c0bbb5d75c98a9d8d61f35322b1614ac8c6
0x57e11a <stella@lifeless.space>
2025-12-28 06:06:54 -0800

    gitignore + nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+		systems.url = "github:nix-systems/default";
+	};
+
+	outputs = { nixpkgs, systems, ... }: let
+		forAllSystems = fn: nixpkgs.lib.genAttrs (import systems) (system: fn nixpkgs.legacyPackages.${system});
+	in {
+		devShells = forAllSystems (pkgs: {
+			default = pkgs.mkShell {
+				buildInputs = with pkgs; [
+					lua52Packages.lua
+					lua52Packages.argparse
+					lua52Packages.luasocket
+					lua52Packages.luasec
+					lua52Packages.dkjson
+				];
+			};
+		});
+	};
+}
